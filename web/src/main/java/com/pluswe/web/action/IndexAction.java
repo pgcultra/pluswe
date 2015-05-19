dfa05765df4d19226486f6b6f3346ab7ab351809
PanGuangchao <pgcultra@gmail.com>
2015-05-19 13:48:54 +0800

    Spring初始化

--- web/src/main/java/com/pluswe/web/action/IndexAction.java
+++ web/src/main/java/com/pluswe/web/action/IndexAction.java
@@ -1,7 +1,18 @@
 package com.pluswe.web.action;
 
-public class IndexAction {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+public class IndexAction {
+	
+	@ResponseBody
+	@RequestMapping({ "/index", "/index/" })
+	public String index(){
+		System.out.println("hello world");
+		return "hello";
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
